a9c8aadd6ba0614f3a82bf97c4e46e75958bd2c5
TheRealShreyash <koshta.shreyash02@gmail.com>
2026-03-09 13:58:54 +0530

    patch: fixed left card sticky behaviour

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import ResumeFab from "./components/ResumeFab";
 const App = () => {
   return (
     <div className="text-white mx-auto my-0 md:border-x-4 md:border-[#262626] border-dashed w-full max-w-6xl p-6 md:p-10 flex flex-col md:flex-row gap-12 md:gap-24 items-start">
-      <div className="w-full md:w-fit shrink-0 animate-fade-in-up"
-      style={{animationDelay: '80ms'}}>
-        <LeftCard className="w-full md:w-fit pp-font shrink-0" />
-      </div>
-      <div className="w-full md:w-auto min-w-0 animate-fade-in-up"
-      style={{animationDelay: '220ms'}}>
+      <LeftCard
+        className="w-full md:w-fit pp-font shrink-0 animate-fade-in-up"
+        style={{ animationDelay: "80ms" }}
+      />
+      <div
+        className="w-full md:w-auto min-w-0 animate-fade-in-up"
+        style={{ animationDelay: "220ms" }}
+      >
         <RightCard className="w-full md:w-auto pp-font min-w-0" />
       </div>
       <ResumeFab />
